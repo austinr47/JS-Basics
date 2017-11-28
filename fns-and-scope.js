bac0877d7f4e88e7fd637d79c6d0eb03b1738fc4
Austin Randall <randall@Austins-MacBook-Pro.local>
2017-11-27 20:08:41 -0700

    fns-and-scope.js done

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -11,6 +11,7 @@ function isTyler(name){
 //////////////////PROBLEM 2////////////////////
 
 
+
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 function getName(){
   var name = prompt('Please enter name here')
